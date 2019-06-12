85b271c1fcb59877c4a7a4ab8ed55387340be71e
Tony <antonbragin28@yandex.ru>
2019-06-12 21:25:17 +0700

    Initial Commit

---
+++ mvvmTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvmTest
+//
+//  Created by TONY on 12/06/2019.
+//  Copyright © 2019 TONY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
